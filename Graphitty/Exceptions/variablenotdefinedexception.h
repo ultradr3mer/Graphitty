3f240b7501418c9a15827497ae33a6c67458460e
Clara Schulz-Theißen <Schulz-Theissen@eevolution.de>
2022-09-24 12:52:35 +0200

    Functions moved

---
+++ Graphitty/Exceptions/variablenotdefinedexception.h
@@ -0,0 +1,21 @@
+#ifndef VARIABLENOTDEFINEDEXCEPTION_H
+#define VARIABLENOTDEFINEDEXCEPTION_H
+
+#include "FunctionParserException.h"
+
+class VariableNotDefinedException : public FunctionParserException
+{
+  // private:
+  // string message;
+public:
+  inline VariableNotDefinedException()
+  {
+    message = "A variable used int this FunctionNode was not defined in the hashmap.";
+  }
+  inline VariableNotDefinedException(string var)
+  {
+    message = string("The variable '") + var + string("' is not a defined variable.");
+  }
+};
+
+#endif // VARIABLENOTDEFINEDEXCEPTION_H
